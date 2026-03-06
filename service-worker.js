c8c29d8d73766a29efddd1701ffac10ca14755f9
pranjalsrivastavaps94-beep <pranjalsrivastava.ps94@gmail.com>
2026-03-06 13:05:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "study-tracker-2026-v1";
+const CACHE_NAME = "study-tracker-2026-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -38,3 +38,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
